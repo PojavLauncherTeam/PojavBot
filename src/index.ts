ce7aebf36aa134ce7b0e20ba70a567376380dc87
Synbulat Biishev <syjalo.dev@gmail.com>
2022-07-16 03:47:05 +0500

    Add welcome messages

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Intents } from 'discord.js';
 import { PojavClient } from './util/PojavClient';
 
 const client = new PojavClient({
-  intents: [Intents.FLAGS.GUILDS],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
 });
 
 client.login();
