1ce035924689b2a380f35c5c67617af7edd403ec
Synbulat Biishev <syjalo.dev@gmail.com>
2022-07-16 15:58:47 +0500

    Add user flags (#9)

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,42 +1,66 @@
 import { Time } from '@sapphire/time-utilities';
-import { type ColorResolvable, Constants, Formatters, MessageEmbed } from 'discord.js';
+import { type ColorResolvable, Constants, Formatters, MessageEmbed, BitField } from 'discord.js';
 import type { PojavEvent } from '.';
+import { UserFlags } from '../util/DatabaseClient';
 import { makeFormattedTime, makeUserURL } from '../util/Util';
 
 export const event: PojavEvent<'guildMemberAdd'> = {
   async listener(client, member) {
-    const dbGuild = await client.database.guilds.findOne({ id: member.guild.id });
-    if (!dbGuild?.joinLeaveChannelId) return;
+    const [dbGuild, dbUser] = await Promise.all([
+      client.database.guilds.findOne({ id: member.guild.id }),
+      client.database.users.findOne({ id: member.id }),
+    ]);
 
-    const joinLeaveChannel = client.channels.resolve(dbGuild.joinLeaveChannelId);
-    if (!joinLeaveChannel?.isText()) return;
+    if (!dbGuild) return;
+    const { joinLeaveChannelId, developerRoleId, contributorRoleId, vipRoleId } = dbGuild;
 
-    const userCreatedAccountAgo = Date.now() - member.user.createdTimestamp;
-    let color: ColorResolvable;
-    if (userCreatedAccountAgo > Time.Day * 28) color = Constants.Colors.GREEN;
-    else if (userCreatedAccountAgo > Time.Day * 14) color = Constants.Colors.YELLOW;
-    else color = Constants.Colors.RED;
+    if (joinLeaveChannelId) {
+      const joinLeaveChannel = client.channels.resolve(joinLeaveChannelId);
+      if (!joinLeaveChannel?.isText()) return;
 
-    const embed = new MessageEmbed()
-      .setAuthor({
-        iconURL: member.displayAvatarURL({ dynamic: true, format: 'png' }),
-        name: member.displayName,
-        url: makeUserURL(member.id),
-      })
-      .setTitle('Welcome!')
-      .setDescription(`${member} ${Formatters.inlineCode(member.user.tag)} (${member.id})`)
-      .setFields(
-        {
-          name: 'Created the account',
-          value: makeFormattedTime(member.user.createdAt),
-        },
-        {
-          name: 'Joined the server',
-          value: makeFormattedTime(member.joinedAt!),
-        }
-      )
-      .setColor(color);
+      const userCreatedAccountAgo = Date.now() - member.user.createdTimestamp;
+      let color: ColorResolvable;
+      if (userCreatedAccountAgo > Time.Day * 28) color = Constants.Colors.GREEN;
+      else if (userCreatedAccountAgo > Time.Day * 14) color = Constants.Colors.YELLOW;
+      else color = Constants.Colors.RED;
 
-    joinLeaveChannel.send({ embeds: [embed] });
+      const embed = new MessageEmbed()
+        .setAuthor({
+          iconURL: member.displayAvatarURL({ dynamic: true, format: 'png' }),
+          name: member.displayName,
+          url: makeUserURL(member.id),
+        })
+        .setTitle('Welcome!')
+        .setDescription(`${member} ${Formatters.inlineCode(member.user.tag)} (${member.id})`)
+        .setFields(
+          {
+            name: 'Created the account',
+            value: makeFormattedTime(member.user.createdAt),
+          },
+          {
+            name: 'Joined the server',
+            value: makeFormattedTime(member.joinedAt!),
+          }
+        )
+        .setColor(color);
+
+      joinLeaveChannel.send({ embeds: [embed] });
+    }
+
+    if (dbUser?.flags) {
+      const bitField = new BitField(dbUser.flags);
+      const roleIdsToAdd = [];
+      if (developerRoleId && bitField.has(UserFlags.Developer)) {
+        roleIdsToAdd.push(developerRoleId);
+      }
+      if (contributorRoleId && bitField.has(UserFlags.Contributor)) {
+        roleIdsToAdd.push(contributorRoleId);
+      }
+      if (vipRoleId && bitField.has(UserFlags.VIP)) {
+        roleIdsToAdd.push(vipRoleId);
+      }
+
+      member.roles.add(roleIdsToAdd);
+    }
   },
 };
--- src/util/DatabaseClient.ts
+++ src/util/DatabaseClient.ts
@@ -4,10 +4,12 @@ import { type Collection, MongoClient, type MongoClientOptions } from 'mongodb';
 export class DatabaseClient {
   public mongo: MongoClient;
   public guilds: Collection<GuildSchema>;
+  public users: Collection<UserSchema>;
 
   public constructor(url: string, options?: MongoClientOptions) {
     this.mongo = new MongoClient(url, options);
     this.guilds = this.mongo.db().collection<GuildSchema>('guilds');
+    this.users = this.mongo.db().collection<UserSchema>('users');
   }
 }
 
@@ -15,4 +17,18 @@ interface GuildSchema {
   id: Snowflake;
   logsChannelId?: Snowflake;
   joinLeaveChannelId?: Snowflake;
+  developerRoleId?: Snowflake;
+  contributorRoleId?: Snowflake;
+  vipRoleId?: Snowflake;
+}
+
+interface UserSchema {
+  id: Snowflake;
+  flags?: string;
+}
+
+export enum UserFlags {
+  Developer = 1 << 0,
+  Contributor = 1 << 1,
+  VIP = 1 << 2,
 }
