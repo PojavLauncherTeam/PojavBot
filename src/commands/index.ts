40efd1739ac32c6f6652373e6843d4fe70c8ca3f
Uni0005 <holandsol6@gmail.com>
2022-07-20 15:59:57 +0000

    new commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,6 +5,8 @@ import type { PojavClient } from '../util/PojavClient';
 export * as status from './chatInput/status';
 export * as tag from './chatInput/tag';
 export * as tags from './chatInput/tags';
+export * as userinfo from './chatInput/userinfo';
+export * as report from './moderation/report';
 
 export interface PojavChatInputCommand {
   data: Pick<SlashCommandBuilder, 'toJSON'>;
