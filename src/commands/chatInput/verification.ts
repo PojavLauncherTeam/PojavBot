6212b2f0fc942c17bdef143e36b1c359b74b38df
Uni <holandsol6@gmail.com>
2023-03-26 21:29:50 +0300

    fix (#80)
    * verification and logparser functions
    
    * Update index.ts
    
    * fixing the github compiler error
    
    * Update index.ts
    
    * update
    
    * fix
    
    ---------
    
    Co-authored-by: Synbulat Biishev <syjalo.dev@gmail.com>

--- src/commands/chatInput/verification.ts
+++ src/commands/chatInput/verification.ts
@@ -8,11 +8,11 @@ export const command: PojavChatInputCommand = {
     .setDescription('Verifies your ownership of Minecraft:Java Edtion!'),
   async listener(interaction, { getString }) {
     await interaction.reply({
-      content: `[${getString(
-        `commands.verification.click`
-      )}](https://login.live.com/oauth20_authorize.srf?client_id=719da651-ebe1-489f-88ab-5ff20f1a74ee&response_type=code&redirect_uri=${
-        process.env.redirectUri
-      }&scope=XboxLive.signin%20offline_access&state=${interaction.user.id}+${interaction.guild.id})`,
+      content: `[${getString(`commands.verification.click`)}](https://login.live.com/oauth20_authorize.srf?client_id=${
+        process.env.clientId
+      }&response_type=code&redirect_uri=${process.env.redirectUri}&scope=XboxLive.signin%20offline_access&state=${
+        interaction.user.id
+      }+${interaction.guild.id})`,
       ephemeral: true,
     });
   },
