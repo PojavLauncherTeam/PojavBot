fc8dda1861a49200a958ffd6998c117f6ec5144d
Uni <holandsol6@gmail.com>
2023-03-26 18:56:01 +0300

    fix

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,14 +1,26 @@
 import { Colors, EmbedBuilder } from 'discord.js';
 import fetch from 'node-fetch';
+import type { GetStringFunctionOptions, PojavStringsFile } from '../util/LocalizationManager';
+import { resolveLocale } from '../util/Util';
 import type { PojavEvent } from '.';
 
 export const event: PojavEvent<'messageCreate'> = {
-  async listener(_client, message) {
+  async listener(client, message) {
     if (message.attachments) {
       const attachment = message.attachments.first();
       if (!attachment) return;
+      if (!message.guild) return;
       if (attachment.name?.slice(-4) !== '.txt') return;
       try {
+        const dbGuild = await client.database.guilds.findOne({ id: message.guild.id });
+        if (!dbGuild) return;
+        function getString(
+          key: keyof PojavStringsFile,
+          { locale = resolveLocale(dbGuild?.locale), variables }: Partial<GetStringFunctionOptions> = {}
+        ) {
+          return client.localizations.getString(key, { locale, variables });
+        }
+
         const response = await fetch(attachment.url);
         const text = await response.text();
         if (text) {
@@ -19,14 +31,22 @@ export const event: PojavEvent<'messageCreate'> = {
           const result: any = await parser.json().catch(() => null);
           if (!result) return;
           const embed = new EmbedBuilder()
-            .setTitle('Log information')
+            .setTitle(`${getString('events.messageCreate.log_information')}`)
             .setDescription(
-              `**Launcher version:** ${result.version.major_code}-${result.version.commit_number}-${result.version.branch}\n**Game Version:** ${result.minecraft_version.name} (${result.minecraft_version.type})\n**Renderer:** ${result.renderer}\n**Java version:** ${result.java_runtime.version}\n**Architecture:** ${result.architecture}`
+              `**${getString('events.messageCreate.launcher_version')}** ${result.version.major_code}-${
+                result.version.commit_number
+              }-${result.version.branch}
+              **${getString('events.messageCreate.game_version')}** ${getString(
+                result.minecraft_version.name
+              )} (${getString(result.minecraft_version.type)})
+              **${getString('events.messageCreate.render')}** ${getString(result.renderer)}
+              **${getString('events.messageCreate.java_version')}** ${getString(result.java_runtime.version)}
+              **${getString('events.messageCreate.architecture')}** ${getString(result.architecture)}`
             )
             .setColor(Colors.Green);
           if (result.errors.length) {
             embed.setColor(Colors.Red);
-            embed.addFields({ name: 'Errors', value: result.errors.join('\n') });
+            embed.addFields({ name: `${getString('events.messageCreate.errors')}`, value: result.errors.join('\n') });
           }
 
           await message.reply({ embeds: [embed] });
